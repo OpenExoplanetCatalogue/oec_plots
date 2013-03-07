8ce92eeb0bc54a0afbdc765a5dcb5d3cb3b215d6
Hanno Rein <hanno@hanno-rein.de>
2013-03-07 10:08:46 -0500

    Unified update scripts

--- update_everything.bash
+++ update_everything.bash
@@ -13,8 +13,23 @@ git pull
 popd
 commit "automatic update: pulling new open_exoplanet_catalogue"
 
-doall.bash
-convert_all_svg_to_png.bash
-commit "automatic update: generateASCIITables"
+## make plots
+python plot_mass_vs_semimajoraxis_discovery.python 
+python plot_mass_vs_semimajoraxis_eccentricity.python
+python plot_periodratio.python
+python plot_architecture.python
+python plot_binarysep_vs_planetsep.python
+python plot_discoveryyear.python
+
+# Convert all svg vector images to rasterized png images.
+# This script is using qlmanage and convert. It only works on a mac, but it seems to produce the best quality renderings.
+for file in *.svg
+do
+	echo "$file"
+	qlmanage -t -s 500 -o . "$file"
+	convert "$file.png" -crop 500x400+0+0 "$file.png"
+done	
+
+commit "automatic update: plots"
 
 #git push
